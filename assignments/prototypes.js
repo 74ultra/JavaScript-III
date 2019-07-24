e6d571137aa3ce486db8b5b95644c8a908eaa9cb
74ultra <r.barnhill@protonmail.com>
2019-07-24 14:28:55 -0700

    Completed 'Prototypes' assigment (non-stretch). MVP met.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,10 +16,10 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
-function GameObject(gameAttrs){
-  this.createdAt = gameAttrs.createdAt;
-  this.name = gameAttrs.name;
-  this.dimensions = gameAttrs.dimensions;
+function GameObject(gameProps){
+  this.createdAt = gameProps.createdAt;
+  this.name = gameProps.name;
+  this.dimensions = gameProps.dimensions;
 }
 
 
@@ -27,15 +27,7 @@ GameObject.prototype.destroy = function(){
   return `${this.name} was removed from the game`
 }
 
-console.log(GameObject);
 
-const starGazer = new GameObject({
-  createdAt: 'Wallsingham Studios',
-  name: 'Star Gazer',
-  dimensions: 5
-})
-
-console.log(starGazer.destroy())
 
 /*
   === CharacterStats ===
@@ -43,6 +35,20 @@ console.log(starGazer.destroy())
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(charProps){
+  GameObject.call(this, charProps);
+  this.healthPoints = charProps.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype)
+
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage`
+}
+
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -53,7 +59,37 @@ console.log(starGazer.destroy())
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(humProps){
+  CharacterStats.call(this, humProps);
+  this.team = humProps.team;
+  this.weapons = humProps.weapons;
+  this.language = humProps.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+/*
+const link = new Humanoid({
+  createdAt: 'Place',
+  name: 'Link',
+  dimensions: {
+    length: 5,
+    width: 5,
+    height: 5,
+  },
+  healthPoints: 100,
+  team: 'Blue Mountain Clan',
+  weapons: 'battle axe',
+  language: 'Sindarin'
+})
+
+console.log(link.greet())
+*/
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -62,7 +98,7 @@ console.log(starGazer.destroy())
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -123,7 +159,7 @@ console.log(starGazer.destroy())
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
